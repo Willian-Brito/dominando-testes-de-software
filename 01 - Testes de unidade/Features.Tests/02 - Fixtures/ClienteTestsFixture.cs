e1f3cef464c1ef43fcce28776a48e20957513701
Willian Brito <willian_brito00@hotmail.com>
2025-03-11 19:56:53 -0300

    test: :white_check_mark: implementando recurso de fixtures para reaproveitamento de dados nos testes

---
+++ 01 - Testes de unidade/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,37 @@
+﻿using Features.Clientes;
+
+namespace Features.Tests.Fixtures;
+
+[CollectionDefinition(nameof(ClienteCollection))]
+public class ClienteCollection : ICollectionFixture<ClienteTestsFixture> { }
+
+public class ClienteTestsFixture : IDisposable
+{
+    public Cliente GerarClienteValido()
+    {
+        return new Cliente(
+            Guid.NewGuid(),
+            "Willian",
+            "Brito",
+            DateTime.Now.AddYears(-30),
+            "will@edu.com",
+            true,
+            DateTime.Now);
+    }
+
+    public Cliente GerarClienteInvalido()
+    {
+        return new Cliente(
+            Guid.NewGuid(),
+            "",
+            "",
+            DateTime.Now,
+            "edu2edu.com",
+            true,
+            DateTime.Now);
+    }
+
+    public void Dispose()
+    {
+    }
+}
